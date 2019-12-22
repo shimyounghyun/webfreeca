00c6b775d78083e561c947072233848b7b1d7f21
shimyounghyun <lhlrol1004@naver.com>
2019-12-22 10:56:01 +0900

    헤더 추가

--- webfreeca-frontend/src/Root.js
+++ webfreeca-frontend/src/Root.js
@@ -2,11 +2,14 @@ import React from "react";
 import App from "./components/App";
 import { Provider } from "react-redux";
 import store from "./store/configure";
+import {BrowserRouter} from "react-router-dom";
 
 const Root = () => {
   return (
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   );
 };
---
+++ webfreeca-frontend/src/pages/Main.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Header from "../components/structure/Header";
+
+const Main = () => {
+    return <Header/>;
+}
+
+export default Main;
