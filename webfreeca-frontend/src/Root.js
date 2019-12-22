76a0641c3e4de4cf2020a55930ee3e03eb648738
shimyounghyun <lhlrol1004@naver.com>
2019-12-22 10:15:45 +0900

    react 프로젝트 구조 잡기

---
+++ webfreeca-frontend/src/Root.js
@@ -0,0 +1,13 @@
+import React from "react";
+import App from "./components/App";
+import { Provider } from "react-redux";
+import store from "./store/configure";
+
+const Root = () => {
+  return (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+};
+export default Root;
--- webfreeca-frontend/src/index.js
+++ webfreeca-frontend/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import "./styles/main.scss"
+import Root from './Root';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<Root/>, document.getElementById('root'));
 
